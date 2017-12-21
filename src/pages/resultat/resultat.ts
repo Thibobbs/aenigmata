11df87ffb717a7e6edd8006b212f9dab753061dc
RENARD Thibaut <thibaut.renard@hetic.net>
2017-12-21 02:12:33 +0100

    :rocket: resultatPage

---
+++ src/pages/resultat/resultat.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+//Pages
+import { AnecdotePage } from '../anecdote/anecdote';
+
+//Provider
+import { NavigationProvider } from '../../providers/navigation/navigation';
+
+/**
+ * Generated class for the ResultatPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-resultat',
+  templateUrl: 'resultat.html',
+})
+export class ResultatPage {
+
+  nextPage = AnecdotePage;
+  private infos;
+  private params;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private navigate: NavigationProvider) {
+    this.infos = navParams.get('infos');
+    this.params = navParams.get('params');
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ResultatPage');
+  }
+
+}
